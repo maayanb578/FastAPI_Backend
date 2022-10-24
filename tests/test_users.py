127b2df7ba364efff28528b1f6da26a4806470d5
Maayan Bitan <maayanb578@gmail.com>
2022-10-24 11:12:21 +0300

    adding first github action

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,16 +1,35 @@
-from fastapi.testclient import TestClient
-from app.main import app
+
+from app import schemas
+import pytest
+from jose import jwt
+from app.config import settings
 
 
-client = TestClient(app)
+def test_create_user(client):
+    res = client.post("/users/", json={"email": "examp@gmail.com", "password": "password123"})
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "examp@gmail.com"
+    assert res.status_code == 201
 
 
-def test_root():
-    res = client.get("/")
-    assert res.json().get('message') == 'Hello World'
+def test_login_user(client, test_user):
+    res = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200
 
-def test_create_user():
-    res = client.post("/users/", json={"email": "hello123@gmail.com", "password": "password123"})
-    print(res.json())
-    assert res.status_code == 201
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wronggmali@gmail.com', 'password123', 403),
+    ('examp@gmail.com', 'worngpassword', 403),
+    ('wronggmali@gmail.com', 'worngpassword', 403),
+    (None, 'password123', 422),
+    ('examp@gmail.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
